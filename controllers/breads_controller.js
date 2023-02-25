dc9384c504276e02d5c884f739bd97d97c7e4aa0
Jake Filipowski <filipowskijm@gmail.com>
2023-02-25 13:37:11 -0500

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,6 +17,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -28,7 +36,6 @@ breads.get('/:arrayIndex', (req, res) => {
     res.render('404')
   }
 })
-
 
 // CREATE
 breads.post('/', (req, res) => {
@@ -48,6 +55,17 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
