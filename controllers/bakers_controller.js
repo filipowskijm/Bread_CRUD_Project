51c7b8bba5c1ed7a0592dc46b73b826ab7f00840
Jake Filipowski <filipowskijm@gmail.com>
2023-03-08 20:00:09 -0500

    added delete bakers hook

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -24,6 +24,14 @@ baker.get('/:id', (req, res) => {
         })
 })
 
+// delete
+baker.delete('/:id', (req, res) => {
+    Baker.findByIdAndDelete(req.params.id) 
+      .then(deletedBaker => { 
+        res.status(303).redirect('/breads')
+      })
+})
+
 // Seed
 baker.get('/data/seed', (req, res) => {
     Baker.insertMany(bakerSeedData)
